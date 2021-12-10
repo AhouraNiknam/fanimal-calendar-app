ac8f8f6816534774ee7ea3823f6a34af62e06ffe
Ahoura <nikna001@cougars.csusm.edu>
2021-12-10 13:57:39 -0800

    calander app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+import { DateRangePicker } from "react-dates";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Fanimal Calendar App </h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    //Initialize beginning values as empty
+    this.state = {
+      startDate: null,
+      endDate: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Fanimal Calender App </h1>
+        <DateRangePicker
+          startDate={this.state.startDate}
+          startDateId="startId"
+          endDate={this.state.endDate}
+          endDateId="endId"
+          onDatesChange={({ startDate, endDate }) =>
+            this.setState({ startDate, endDate })
+          }
+          focusedInput={this.state.focusedInput}
+          onFocusChange={(focusedInput) => this.setState({ focusedInput })}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
