90d5fa7d4cd4c9ba747f30703b0d4496942c716d
Ahoura <nikna001@cougars.csusm.edu>
2021-12-15 14:29:29 -0800

    Enabled calendar to remain open after date selection

--- src/App.js
+++ src/App.js
@@ -4,19 +4,92 @@ import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
 import { DateRangePicker } from "react-dates";
 import "./App.css";
+import moment from "moment";
+import isInclusivelyAfterDay from "react-dates/lib/utils/isInclusivelyAfterDay";
+import omit from "lodash/omit";
 
+const defaultProps = {
+  initialVisibleMonth: null,
+  numberOfMonths: 2,
+  //This keeps the calendar open after date selection
+  keepOpenOnDateSelect: true,
+  reopenPickerOnClearDates: true,
+  minimumNights: 1,
+  enableOutsideDays: true,
+  isDayBlocked: () => false,
+  isOutsideRange: (day) => !isInclusivelyAfterDay(day, moment()),
+  isDayHighlighted: () => false,
+
+  // internationalization
+  displayFormat: () => moment.localeData().longDateFormat("L"),
+  monthFormat: "MMMM YYYY",
+  stateDateWrapper: (date) => date,
+};
 class App extends Component {
   constructor(props) {
     super(props);
-    //Initialize beginning values as empty
+
+    let focusedInput = null;
+    if (props.autoFocus) {
+      focusedInput = this.startDate;
+    } else if (props.autoFocusEndDate) {
+      focusedInput = this.endDate;
+    }
     this.state = {
-      startDate: null,
-      endDate: null,
+      focusedInput,
+      startDate: props.initialStartDate,
+      endDate: props.initialEndDate,
     };
+    this.onDatesChange = this.onDatesChange.bind(this);
+    this.onFocusChange = this.onFocusChange.bind(this);
+  }
+
+  onDatesChange({ startDate, endDate }) {
+    const { stateDateWrapper } = this.props;
+    this.setState({
+      startDate: startDate && stateDateWrapper(startDate),
+      endDate: endDate && stateDateWrapper(endDate),
+    });
+  }
+
+  onFocusChange(focusedInput) {
+    this.setState({ focusedInput });
   }
 
   render() {
+    const { focusedInput, startDate, endDate } = this.state;
+    const props = omit(this.props, [
+      "autoFocus",
+      "autoFocusEndDate",
+      "initialStartDate",
+      "initialEndDate",
+      "stateDateWrapper",
+    ]);
+
     return (
+      <div className="App">
+        <h1 className="Align"> Fanimal Calender App </h1>
+        <DateRangePicker
+          {...props}
+          onDatesChange={this.onDatesChange}
+          onFocusChange={this.onFocusChange}
+          focusedInput={focusedInput}
+          startDate={this.state.startDate}
+          startDateId="your_unique_start_date_id"
+          endDate={this.state.endDate}
+          endDateId="your_unique_end_date_id"
+          //Don't know why this is causing an error
+          //If I change the name, the program crashes
+          onDatesChange={({ startDate, endDate }) =>
+            this.setState({ startDate, endDate })
+          }
+          focusedInput={this.state.focusedInput}
+          onFocusChange={(focusedInput) => this.setState({ focusedInput })}
+        />
+      </div>
+      //#region Previous Code
+      //Calendar closes after date selection
+      /*
       <div className="App">
         <h1 className="Align"> Fanimal Calender App </h1>
         <DateRangePicker
@@ -31,8 +104,12 @@ class App extends Component {
           onFocusChange={(focusedInput) => this.setState({ focusedInput })}
         />
       </div>
+      */
+      //#endregion
     );
   }
 }
+
+DateRangePicker.defaultProps = defaultProps;
 
 export default App;
