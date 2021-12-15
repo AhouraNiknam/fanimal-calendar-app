fa15278ee6dc0c3454bfd98e29dffb5e36be4937
Ahoura <nikna001@cougars.csusm.edu>
2021-12-15 15:25:51 -0800

    Enable user to select dates in the past

--- src/App.js
+++ src/App.js
@@ -4,27 +4,20 @@ import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
 import { DateRangePicker } from "react-dates";
 import "./App.css";
-import moment from "moment";
-import isInclusivelyAfterDay from "react-dates/lib/utils/isInclusivelyAfterDay";
-import omit from "lodash/omit";
 
 const defaultProps = {
-  initialVisibleMonth: null,
   numberOfMonths: 2,
   //This keeps the calendar open after date selection
   keepOpenOnDateSelect: true,
   reopenPickerOnClearDates: true,
   minimumNights: 1,
   enableOutsideDays: true,
-  isDayBlocked: () => false,
-  isOutsideRange: (day) => !isInclusivelyAfterDay(day, moment()),
-  isDayHighlighted: () => false,
-
-  // internationalization
-  displayFormat: () => moment.localeData().longDateFormat("L"),
-  monthFormat: "MMMM YYYY",
-  stateDateWrapper: (date) => date,
+  //This enables user to select past dates
+  isOutsideRange() {},
+  autoFocus: false,
+  autoFocusEndDate: false,
 };
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -45,7 +38,7 @@ class App extends Component {
   }
 
   onDatesChange({ startDate, endDate }) {
-    const { stateDateWrapper } = this.props;
+    const { stateDateWrapper } = this.def;
     this.setState({
       startDate: startDate && stateDateWrapper(startDate),
       endDate: endDate && stateDateWrapper(endDate),
@@ -57,29 +50,20 @@ class App extends Component {
   }
 
   render() {
-    const { focusedInput, startDate, endDate } = this.state;
-    const props = omit(this.props, [
-      "autoFocus",
-      "autoFocusEndDate",
-      "initialStartDate",
-      "initialEndDate",
-      "stateDateWrapper",
-    ]);
+    const { isOutsideRange } = this.props;
+    const { focusedInput } = this.state;
 
     return (
       <div className="App">
         <h1 className="Align"> Fanimal Calender App </h1>
         <DateRangePicker
-          {...props}
+          isOutsideRange={isOutsideRange}
           onDatesChange={this.onDatesChange}
           onFocusChange={this.onFocusChange}
           focusedInput={focusedInput}
           startDate={this.state.startDate}
-          startDateId="your_unique_start_date_id"
           endDate={this.state.endDate}
-          endDateId="your_unique_end_date_id"
-          //Don't know why this is causing an error
-          //If I change the name, the program crashes
+          //Don't know why this is causing an error, program still works though
           onDatesChange={({ startDate, endDate }) =>
             this.setState({ startDate, endDate })
           }
@@ -87,25 +71,6 @@ class App extends Component {
           onFocusChange={(focusedInput) => this.setState({ focusedInput })}
         />
       </div>
-      //#region Previous Code
-      //Calendar closes after date selection
-      /*
-      <div className="App">
-        <h1 className="Align"> Fanimal Calender App </h1>
-        <DateRangePicker
-          startDate={this.state.startDate}
-          startDateId="startId"
-          endDate={this.state.endDate}
-          endDateId="endId"
-          onDatesChange={({ startDate, endDate }) =>
-            this.setState({ startDate, endDate })
-          }
-          focusedInput={this.state.focusedInput}
-          onFocusChange={(focusedInput) => this.setState({ focusedInput })}
-        />
-      </div>
-      */
-      //#endregion
     );
   }
 }
