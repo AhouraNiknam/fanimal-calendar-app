dee90fa188a3593bd8be872925c82a8f4f182c48
Ahoura <nikna001@cougars.csusm.edu>
2021-12-15 12:58:07 -0800

    Aligned items to the center of screen

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Fanimal Calender App </h1>
+        <h1 className="Align"> Fanimal Calender App </h1>
         <DateRangePicker
           startDate={this.state.startDate}
           startDateId="startId"
